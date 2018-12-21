a49ff93ed8e7042ed96e21d2073de54384d468bf
bluebluerice <18316961025@163.com>
2018-12-21 17:11:03 +0800

    编译

---
+++ dist/src/request.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function default_1(obj) {
+    return function (target, name, descriptor) {
+        const fn = descriptor.value;
+        descriptor.value = (router) => {
+            router[obj.method](obj.url, async (ctx, next) => {
+                await fn(ctx, next);
+            });
+        };
+    };
+}
+exports.default = default_1;
+//# sourceMappingURL=request.js.map
